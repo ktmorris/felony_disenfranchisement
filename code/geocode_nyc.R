f0065a8dece0d4e3f18cca94c39ef7da68cc9b86
Kevin Morris <morrisk@brennan.law.nyu.edu>
2019-04-25 22:23:19 -0400

    get rid of arrests data and individual level because I don't think I'm going to use it. If I can think of a creative way to do individual-level turnout stats, this might be helpful, but for now going in a different direction - does the removal of a voter correlate with fewer ballots cast in a block group?

--- code/geocode_nyc.R
+++ code/geocode_nyc.R
@@ -38,62 +38,7 @@
 
 nyc <- readRDS("./temp/nyc.rds")
 
-## pull in arrests data
-arrests <- fread("./raw_data/NYPD_Arrests_Data__Historic_.csv") %>% 
-  filter(LAW_CAT_CD == "F")
-bg_shp <- readOGR("./raw_data/shapefiles/tl_2018_36_bg", "tl_2018_36_bg")
-pings  <- SpatialPoints(arrests[c('Longitude','Latitude')], proj4string = bg_shp@proj4string)
-arrests$bg <- over(pings, bg_shp)$GEOID
 
-arrests <- arrests %>% 
-  group_by(bg) %>% 
-  tally()
-
-##
-income <- rbindlist(lapply(c("KINGS", "QUEENS", "BRONX", "NEW YORK", "RICHMOND"), function(c){
-  income <- census_income("block group", state = "NY", year = 2017, county = c) %>% 
-    select(-NAME)
-}))
-
-race <- rbindlist(lapply(c("KINGS", "QUEENS", "BRONX", "NEW YORK", "RICHMOND"), function(c){
-  race <- census_race_ethnicity("block group", state = "NY", year = 2017, county = c) %>% 
-    select(-NAME)
-}))
-
-education <- rbindlist(lapply(c("KINGS", "QUEENS", "BRONX", "NEW YORK", "RICHMOND"), function(c){
-  education <- census_education("block group", state = "NY", year = 2017, county = c) %>% 
-    select(-NAME)
-}))
-
-
-nyc <- left_join(nyc, arrests, by = "bg")
-nyc <- left_join(nyc, income, by = c("bg" = "GEOID"))
-nyc <- left_join(nyc, race, by = c("bg" = "GEOID"))
-nyc <- left_join(nyc, education, by = c("bg" = "GEOID"))
-nyc <- nyc %>% 
-  mutate(arrests = ifelse(is.na(n), 0, n),
-         arrests_pop = arrests / population) %>% 
-  select(-n)
-
-rm(bg_shp, arrests, education, income, pings, race)
-
-nyc_s <- nyc[complete.cases(nyc), ]
-
-bg_count <- length(unique(nyc_s$bg))
-
-bgs <- nyc_s %>% 
-  group_by(bg) %>% 
-  filter(row_number() == 1) %>% 
-  arrange(arrests_pop) %>% 
-  ungroup() %>% 
-  mutate(control = (row_number() / bg_count) < 0.5,
-         treat = (row_number() / bg_count) > 0.9) %>% 
-  filter(control == T | treat == T) %>% 
-  select(bg, treat, control)
-
-nyc_s <- filter(nyc_s, bg %in% bgs$bg)
-nyc_s <- left_join(nyc_s, bgs, by = "bg") %>% 
-  select(nys_id, party, gender, age, median_income, latino, nh_black, nh_white, some_college, treat, control, arrest_pop)
 
 ### block group level
 
