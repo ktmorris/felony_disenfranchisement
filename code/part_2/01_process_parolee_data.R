175a3a49f85c1b9720bd1e70b6897d3ad2a2bc84
Kevin <kevin.morris@nyu.edu>
2019-05-23 17:52:34 -0400

    add layer to code structure

---
+++ code/part_2/01_process_parolee_data.R
@@ -0,0 +1,21 @@
+#######################################################################################################################################
+# READ IN DOCCS DATA
+
+names = fread("./raw_data/doccs_data/parolee_column_names.csv")$V1 # VARIABLE NAMES SET IN CSV 'CAUSE I'M LAZY
+
+parolees <- read_fwf(
+  file ="./raw_data/doccs_data/parolee_data/Parolee.011419.txt",
+  fwf_widths(c(1, 9, 3, 10, 3, 7, 3, 30, 3, 6, 3, 10, 3, 15, 3,
+               19, 3, 11, 3, 1, 3, 22, 3, 10, 3, 15, 2))
+)
+
+colnames(parolees) = names
+rm(names)
+
+parolees <- parolees %>% 
+  filter(status == "ACTIVE" | 
+           (status == "DISCHARGED" & status_date >= "2018-01-01")) %>% 
+  select(din)
+
+
+fwrite(parolees, "./temp/parolees.csv")
