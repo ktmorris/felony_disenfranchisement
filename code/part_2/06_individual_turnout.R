5a8aec40be3d4464bd75e033305c6e227e4f5929
Kevin <kevin.morris@nyu.edu>
2019-06-06 16:02:10 -0400

    change reg showing instrument quality dates

--- code/part_2/06_individual_turnout.R
+++ code/part_2/06_individual_turnout.R
@@ -16,16 +16,17 @@ parolees <- left_join(parolees, nys_roll, by = "din") %>%
          finished_post = parole_status_date >= "2018-05-21",
          days_since_done = as.numeric(as.Date("2018-11-06") - parole_status_date),
          days2 = days_since_done^2,
-         restored = ifelse(is.na(restored), F, restored))
+         restored = ifelse(is.na(restored), F, restored),
+         days_since_m21 = parole_status_date - as.Date("2018-05-21"))
 
-model1 <- glm(v2018 ~ finished_post + days_since_done + days2,
+model1 <- glm(v2018 ~ restored + days_since_done + days2,
               family = "binomial", data = parolees)
 
-model2 <- glm(v2018 ~ finished_post + days_since_done + days2 +
+model2 <- glm(v2018 ~ restored + days_since_done + days2 +
                 as.factor(county) + as.factor(race) + as.factor(sex) + age,
               family = "binomial", data = parolees)
 
-model3 <- glm(v2018 ~ finished_post + days_since_done + days2 +
+model3 <- glm(v2018 ~ restored + days_since_done + days2 +
                 as.factor(county) + as.factor(race) + as.factor(sex) + age +
                 felony_a + felony_b + felony_c + felony_d + felony_e + counts + parole_time,
               family = "binomial", data = parolees)
@@ -39,19 +40,19 @@ date_to_model_notime <- glm(v2018 ~
                        as.factor(county) + as.factor(race) + as.factor(sex) + age +
                        felony_a + felony_b + felony_c + felony_d + felony_e + counts + parole_time,
                      family = "binomial",
-                     data = filter(parolees, year(parole_status_date) >= 2017, parole_status_date < "2018-05-21"))
+                     data = filter(parolees, year(parole_status_date) >= 2017, parole_status_date < "2018-04-18"))
 
 date_to_model_time <- glm(v2018 ~ days_since_done + days2 +
                               as.factor(county) + as.factor(race) + as.factor(sex) + age +
                               felony_a + felony_b + felony_c + felony_d + felony_e + counts + parole_time,
                             family = "binomial",
-                            data = filter(parolees, year(parole_status_date) >= 2017, parole_status_date < "2018-05-21"))
+                            data = filter(parolees, year(parole_status_date) >= 2017, parole_status_date < "2018-04-18"))
 
 anova(date_to_model_notime, date_to_model_time, test = "Chisq")
 
 
 save(date_to_model_notime, date_to_model_time, file = "./temp/short_term_to.rdata")
-
+##
 iv1 <- ivreg(v2018 ~ restored | . -restored + finished_post,
              data = filter(parolees, year(parole_status_date) >= 2017))
 
@@ -66,6 +67,10 @@ iv3 <- ivreg(v2018 ~ restored +
 
 save(iv1, iv2, iv3, file = "./temp/iv_individual_turnout_18.rdata")
 
+to_restored <- mean(filter(parolees, restored)$v2018)
+
+saveRDS(to_restored, "./temp/to_restored.rds")
+
 ### 2016
 
 nys_roll <- readRDS("./temp/parolee_to_18.rds")
@@ -75,7 +80,7 @@ parolees <- readRDS("./temp/parolees_with_restoration.rds")
 parolees <- left_join(parolees, nys_roll, by = "din") %>% 
   select(-history, -year, -election_type) %>% 
   filter(parole_status == "DISCHARGED",
-         parole_status_date >= "2010-01-01",
+         parole_status_date >= "2015-01-01",
          parole_status_date <= "2016-10-14",
          !is.na(dob_parole)) %>% 
   mutate(age = as.numeric((as.Date("2016-11-08") - dob_parole) / 365.25),
