19900da3ef59774f1b6a4cf31d4e279190cae53c
Kevin <kevin.morris@nyu.edu>
2019-06-17 15:48:06 -0400

    uncomment

--- code/part_2/05_turnout_trends.R
+++ code/part_2/05_turnout_trends.R
@@ -1,36 +1,36 @@
-# 
-# elects <- fread("./raw_data/misc/elects.csv")
-# 
-# ############# READ IN NYS VOTER FILES #############
-# db <- dbConnect(SQLite(), "D:/rolls.db")
-# nys_roll <- dbGetQuery(db, "select history, voter_status, nys_id from nys_roll_0319")
-# 
-# nys_roll <- nys_roll[nys_roll$nys_id %in% readRDS("./temp/din_nys_parolees.rds")$nys_id, ]
-# 
-# nys_roll <- nys_roll %>%
-#   mutate(a = as.integer(voter_status == "ACTIVE"),
-#          b = as.integer(voter_status == "INACTIVE"),
-#          c = as.integer(voter_status == "PREREG"))
-# 
-# nys_roll <- setorder(nys_roll, nys_id, -a, -b, -c) ## KEEP ONE RECORD FOR EVERY VOTER
-# nys_roll <- nys_roll[!duplicated(nys_roll$nys_id),]
-# nys_roll <- select(nys_roll, -a, -b, -c)
-# 
-# nys_roll <- left_join(nys_roll, readRDS("./temp/din_nys_parolees.rds"), by = "nys_id")
-# 
-# nys_roll <- cSplit(nys_roll, "history", sep = ";", direction = "long", type.convert = F)
-# 
-# nys_roll <- left_join(nys_roll, elects, by = "history")
-# 
-# 
-# nys_roll <- nys_roll %>%
-#   group_by(nys_id) %>%
-#   mutate(year = ifelse(is.na(year), 0, year),
-#          v2018 = max(year == 2018 & election_type == "general"),
-#          v2016 = max(year == 2016 & election_type == "general")) %>%
-#   filter(row_number() == 1)
-# 
-# saveRDS(nys_roll, "./temp/parolee_to_18.rds")
+
+elects <- fread("./raw_data/misc/elects.csv")
+
+############# READ IN NYS VOTER FILES #############
+db <- dbConnect(SQLite(), "D:/rolls.db")
+nys_roll <- dbGetQuery(db, "select history, voter_status, nys_id from nys_roll_0319")
+
+nys_roll <- nys_roll[nys_roll$nys_id %in% readRDS("./temp/din_nys_parolees.rds")$nys_id, ]
+
+nys_roll <- nys_roll %>%
+  mutate(a = as.integer(voter_status == "ACTIVE"),
+         b = as.integer(voter_status == "INACTIVE"),
+         c = as.integer(voter_status == "PREREG"))
+
+nys_roll <- setorder(nys_roll, nys_id, -a, -b, -c) ## KEEP ONE RECORD FOR EVERY VOTER
+nys_roll <- nys_roll[!duplicated(nys_roll$nys_id),]
+nys_roll <- select(nys_roll, -a, -b, -c)
+
+nys_roll <- left_join(nys_roll, readRDS("./temp/din_nys_parolees.rds"), by = "nys_id")
+
+nys_roll <- cSplit(nys_roll, "history", sep = ";", direction = "long", type.convert = F)
+
+nys_roll <- left_join(nys_roll, elects, by = "history")
+
+
+nys_roll <- nys_roll %>%
+  group_by(nys_id) %>%
+  mutate(year = ifelse(is.na(year), 0, year),
+         v2018 = max(year == 2018 & election_type == "general"),
+         v2016 = max(year == 2016 & election_type == "general")) %>%
+  filter(row_number() == 1)
+
+saveRDS(nys_roll, "./temp/parolee_to_18.rds")
 
 nys_roll <- readRDS("./temp/parolee_to_18.rds")
 parolees <- readRDS("./temp/parolees_with_restoration.rds")
