a5e0d948ed7f09cf8a728ba26170e1592b550bc2
Kevin <kevin.morris@nyu.edu>
2019-07-02 17:24:44 -0400

    add plus / minus 35 days to dob

--- code/part_2/04_match_parolees_voter.R
+++ code/part_2/04_match_parolees_voter.R
@@ -2,7 +2,7 @@
 
 ############# READ IN NYS VOTER FILES #############
 db <- dbConnect(SQLite(), "D:/rolls.db")
-nys_roll <- dbGetQuery(db, "select last_name, first_name, middle_name, dob, voter_status, nys_id from nys_roll_0319")
+nys_roll <- dbGetQuery(db, "select last_name, first_name, middle_name, dob, voter_status, nys_id from nys_roll_0319 limit 1000")
 
 nys_roll <- nys_roll %>% 
   mutate(a = as.integer(voter_status == "ACTIVE"),
@@ -32,6 +32,32 @@ small <- merge_list[[1]] %>%
   filter(!is.na(nys_id), !is.na(din))
   
 saveRDS(small, "./temp/din_nys_parolees.rds")
+rm(small, merge_list)
+
+#### plus minus 35 ####
+
+doccs_to_rolls <- doccs_to_rolls %>% 
+  mutate(dob = dob + 35)
+
+merge_list <- match_rolls_to_doc(doccs_to_rolls, din, nys_roll, nys_id)
+
+small <- merge_list[[1]] %>% 
+  select(nys_id, din) %>% 
+  filter(!is.na(nys_id), !is.na(din))
+
+saveRDS(nrow(small), "./temp/p35_2.rds")
+rm(small, merge_list)
+##
+doccs_to_rolls <- doccs_to_rolls %>% 
+  mutate(dob = dob - 70)
+
+merge_list <- match_rolls_to_doc(doccs_to_rolls, din, nys_roll, nys_id)
+
+small <- merge_list[[1]] %>% 
+  select(nys_id, din) %>% 
+  filter(!is.na(nys_id), !is.na(din))
+
+saveRDS(nrow(small), "./temp/m35_2.rds")
 rm(small, merge_list)
 
 cleanup()
