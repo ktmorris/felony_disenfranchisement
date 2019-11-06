54d44d763a4103547aac08643fa824c6b5b2a0d5
Kevin <kevin.morris@nyu.edu>
2019-11-06 12:18:11 -0500

    change to averages for treated and untreated

--- code/part_1/11_bg_facts.R
+++ code/part_1/11_bg_facts.R
@@ -2,17 +2,17 @@
 
 block_groups <- readRDS("./temp/block_group_pre_match.rds")
 
-count_gt_90_black <- sum(block_groups$nh_black > 0.9)
+count_gt_90_black <- sum(block_groups$nh_black > 0.38)
 saveRDS(count_gt_90_black, "./temp/count_gt_90_black")
 
-vap_gt_90_black <- mean(filter(block_groups, nh_black > 0.9)$cvap)
+vap_gt_90_black <- mean(filter(block_groups, nh_black > 0.38)$cvap)
 saveRDS(vap_gt_90_black, "./temp/vap_gt_90_black")
 
-count_lt_10_black <- sum(block_groups$nh_black > 0.9)
+count_lt_10_black <- sum(block_groups$nh_black > 0.38)
 saveRDS(count_gt_90_black, "./temp/count_gt_90_black")
 
-vap_lt_10_black <- mean(filter(block_groups, nh_black < 0.1)$cvap)
-saveRDS(vap_gt_90_black, "./temp/vap_gt_90_black")
+vap_lt_10_black <- mean(filter(block_groups, nh_black < 0.16)$cvap)
+saveRDS(vap_lt_10_black, "./temp/vap_lt_10_black")
 
 ### to stats
 
@@ -24,7 +24,7 @@ to_bgs <- block_groups %>%
 model_a <- lm(to ~ b +
                 median_income + some_college + median_age + 
                 reg_rate + share_dem + share_non_citizen + 
-                share_winner, weights = vap,
+                share_winner, weights = cvap,
               data = to_bgs)
 
 model_b <- data.frame(summary(lm_robust(to ~ b * treat,
